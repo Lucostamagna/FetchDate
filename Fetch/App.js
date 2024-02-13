eeefb0abf41378fa76da80e207a5078e985ffef6
Lucía Costamagna <luci3220@hotmail.com>
2024-02-12 21:12:09 -0300

    add fetch project

--- Fetch/App.js
+++ Fetch/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text style={styles.text}>hdgfhghfghdfghfg</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:50,
+    fontWeight:'600'
+  }
 });
