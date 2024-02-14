90441750ab88f032ef1310441bb8b02faabd843a
Lucía Costamagna <luci3220@hotmail.com>
2024-02-14 00:09:43 -0300

    implemented useSWR

--- Fetch/src/Components/NewList.js
+++ Fetch/src/Components/NewList.js
@@ -1,33 +1,34 @@
-import * as React from 'react'
-import {FlatList, Text, View, StyleSheet} from 'react-native'
-import { useFetch } from '../Hooks/useFetch'
-import NewItens from './NewItens'
+import * as React from "react";
+import { FlatList, Text, View, StyleSheet } from "react-native";
+import { useFetch } from "../Hooks/useFetch";
+import NewItens from "./NewItens";
+import useSWR from "swr";
 
-
-const url= 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+const url = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN";
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const NewList = () => {
-    const {isLoading, error, responseJSON}=useFetch(url)
-  
-  if(isLoading) return <Text> Loading</Text>
-  if(error) return <Text> Error</Text>
-    return (
+  // const {isLoading, error, responseJSON}=useFetch(url)
+  const { data, error } = useSWR(url, fetcher);
+  if (!data) return <Text> Loading</Text>;
+  // if(isLoading) return <Text> Loading</Text>
+  if (error) return <Text> Error</Text>;
+  return (
     <>
-    
-   <FlatList
-   
-   data={responseJSON.Data}
-   renderItem={({item})=> <NewItens {...item}/>}
-   keyExtractor={item=>item.id}
-   ItemSeparatorComponent={()=><View style={{borderBottonWidth: StyleSheet.hairlineWidth}}/> }
-   contentContainerStyle={{paddingVertical:50}}
-   ListHeaderComponent={()=><Text style={{fontSize:32, fontWeight:'bold'}}>News </Text>}
-   />
-
-    
-    
+      <FlatList
+        data={data.Data}
+        renderItem={({ item }) => <NewItens {...item} />}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+          <View style={{ borderBottonWidth: StyleSheet.hairlineWidth }} />
+        )}
+        contentContainerStyle={{ paddingVertical: 50 }}
+        ListHeaderComponent={() => (
+          <Text style={{ fontSize: 32, fontWeight: "bold" }}>News </Text>
+        )}
+      />
     </>
-  )
-}
+  );
+};
 
-export default NewList
+export default NewList;
